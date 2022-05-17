9e50e7a51f373e72a37c137ecb04e1833379c416
kanghao <1084920778@qq.com>
2022-05-17 22:23:12 +0800

    单元测试

---
+++ src/main/java/com/kanghao/publicschooltest/PublicschooltestApplication.java
@@ -0,0 +1,13 @@
+package com.kanghao.publicschooltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicschooltestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublicschooltestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kanghao/publicschooltest/PublicschooltestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanghao.publicschooltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicschooltestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
